55ac34441ba8dd11d01d9f7acbffa1aaf5654b90
Øistein Sørensen <sorenso@gmail.com>
2016-05-31 20:37:36 +0200

    Fixed file ref.

--- launch-configurations/launch-config-node.sh
+++ launch-configurations/launch-config-node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Fetch the file, make it runnable, run it and remove it.
-curl -o ./server-boot-node.sh https://raw.githubusercontent.com/5orenso/aws-ami-creation/master/server-boot-node.sh
+curl -o ./server-boot-node.sh https://raw.githubusercontent.com/5orenso/aws-ami-creation/master/server-boot/server-boot-node.sh
 chmod u+x ./server-boot-node.sh
 bash ./server-boot-node.sh
 rm ./server-boot-node.sh
