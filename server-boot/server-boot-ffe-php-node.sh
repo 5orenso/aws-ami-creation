f947eaf9f96c748f5d31ff88de444fc80d1d7985
Øistein Sørensen <sorenso@gmail.com>
2016-06-06 15:18:09 +0200

    Could be not be equal to null.

--- server-boot/server-boot-ffe-php-node.sh
+++ server-boot/server-boot-ffe-php-node.sh
@@ -6,7 +6,7 @@ export LC_ALL=en_US.UTF-8
 INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
 #aws ec2 associate-address --instance-id $INSTANCE_ID --public-ip $ELASTIC_IP --allow-reassociation --region eu-west-1
 CURRENT_INSTANCE_ID=$(/usr/bin/aws ec2 describe-addresses --allocation-ids $ELASTIC_IP_ALLOCATION_ID --region eu-west-1 | jq -r '.Addresses[].InstanceId')
-if [ -z "$CURRENT_INSTANCE_ID" ]; then
+if [ "$CURRENT_INSTANCE_ID" -ne "null" ]; then
     /usr/bin/aws ec2 associate-address --instance-id $INSTANCE_ID --allocation-id $ELASTIC_IP_ALLOCATION_ID --allow-reassociation --region eu-west-1
 fi
 
