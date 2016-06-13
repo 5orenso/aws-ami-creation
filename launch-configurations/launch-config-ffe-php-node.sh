7916f022e880ae0e13023005bf7452a595d41327
Øistein Sørensen <sorenso@gmail.com>
2016-06-13 18:13:48 +0200

    Simplified files.

--- launch-configurations/launch-config-ffe-php-node.sh
+++ launch-configurations/launch-config-ffe-php-node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Fetch the file, make it runnable, run it and remove it.
-curl -o ./server-boot-ffe-php-node.sh https://raw.githubusercontent.com/5orenso/aws-ami-creation/master/server-boot/server-boot-ffe-php-node.sh
-chmod u+x ./server-boot-ffe-php-node.sh
-bash ./server-boot-ffe-php-node.sh
-rm ./server-boot-ffe-php-node.sh
+curl -o ./server-boot.sh https://raw.githubusercontent.com/5orenso/aws-ami-creation/master/server-boot/server-boot-ffe-php-node.sh
+chmod u+x ./server-boot.sh
+bash ./server-boot.sh
+rm ./server-boot.sh
