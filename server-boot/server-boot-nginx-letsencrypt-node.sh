74f66efd2df7b842bbc1522fbc0d9396e9981a17
Øistein Sørensen <sorenso@gmail.com>
2016-06-14 10:12:19 +0200

    First test of new boot file.

--- server-boot/server-boot-nginx-letsencrypt-node.sh
+++ server-boot/server-boot-nginx-letsencrypt-node.sh
@@ -117,44 +117,44 @@ service node-express-boilerplate start
 
 
 # Nginx and letsencrypt setup
-cat > /etc/nginx/sites-available/default <<'EOF'
-# Inside the server block, add this location block:
-# Add to SSL server block
-        location ~ /.well-known {
-                allow all;
-        }
-EOF
+#cat > /etc/nginx/sites-available/default <<'EOF'
+## Inside the server block, add this location block:
+## Add to SSL server block
+#        location ~ /.well-known {
+#                allow all;
+#        }
+#EOF
 service nginx reload
 
 
-cd /opt/certbot
-./certbot-auto certonly --standalone --email admin@example.com -a webroot --webroot-path=/usr/share/nginx/html -d example.com -d www.example.com -d other.example.net
+#cd /opt/certbot
+#./certbot-auto certonly --standalone --email admin@example.com -a webroot --webroot-path=/usr/share/nginx/html -d example.com -d www.example.com -d other.example.net
 
 
-cat > /etc/nginx/sites-available/default <<'EOF'
-listen 443 ssl;
-
-        server_name example.com www.example.com;
-
-        ssl_certificate /etc/letsencrypt/live/example.com/fullchain.pem;
-        ssl_certificate_key /etc/letsencrypt/live/example.com/privkey.pem;
-
- ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-        ssl_prefer_server_ciphers on;
-        ssl_dhparam /etc/ssl/certs/dhparam.pem;
-        ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
-        ssl_session_timeout 1d;
-        ssl_session_cache shared:SSL:50m;
-        ssl_stapling on;
-        ssl_stapling_verify on;
-        add_header Strict-Transport-Security max-age=15768000;
-
-server {
-    listen 80;
-    server_name example.com www.example.com;
-    return 301 https://$host$request_uri;
-}
-EOF
+#cat > /etc/nginx/sites-available/default <<'EOF'
+#listen 443 ssl;
+#
+#        server_name example.com www.example.com;
+#
+#        ssl_certificate /etc/letsencrypt/live/example.com/fullchain.pem;
+#        ssl_certificate_key /etc/letsencrypt/live/example.com/privkey.pem;
+#
+# ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+#        ssl_prefer_server_ciphers on;
+#        ssl_dhparam /etc/ssl/certs/dhparam.pem;
+#        ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
+#        ssl_session_timeout 1d;
+#        ssl_session_cache shared:SSL:50m;
+#        ssl_stapling on;
+#        ssl_stapling_verify on;
+#        add_header Strict-Transport-Security max-age=15768000;
+#
+#server {
+#    listen 80;
+#    server_name example.com www.example.com;
+#    return 301 https://$host$request_uri;
+#}
+#EOF
 
 service nginx reload
 
