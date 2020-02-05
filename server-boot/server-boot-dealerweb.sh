61034ae40eb051d12dd539bf9e59d22665c446c3
Oistein Sorensen <sorenso@gmail.com>
2020-02-05 09:51:11 +0100

    Updated boot script for dealerweb

--- server-boot/server-boot-dealerweb.sh
+++ server-boot/server-boot-dealerweb.sh
@@ -151,108 +151,16 @@ chmod u+w /var/run/node-ffe-web/
 
 # node app/server.js -c /srv/config/node-ffe-web/config.js
 
-# Startup script
-cat > /etc/init/Zu-CMS.conf <<'EOF'
-# ----------------------------------------------------------------------
-# Zu-CMS - instance
-#
-description     "Zu-CMS"
+cat > /etc/systemd/system/node-ffe-web.service <<EOF
+[Unit]
+Description=node-ffe-web
 
-start on (virtual-filesystems and net-device-up IFACE=eth0)
-stop on runlevel [06]
-
-respawn
-respawn limit 10 5    # Die if respawn more than 10 times in 5 sec.
-
-chdir /srv/Zu-CMS
-setuid ubuntu
-setgid ubuntu
-console log
-
-script
-    echo $$ > /var/run/Zu-CMS/Zu-CMS.pid
-    exec /usr/local/bin/node /srv/Zu-CMS/server.js -c /srv/config/ffe/Zu-CMS/zu/config.js  >> /var/log/Zu-CMS/Zu-CMS.log 2>&1
-end script
-
-pre-start script
-    # Date format same as (new Date()).toISOString() for consistency
-    echo "[`date -u +%Y-%m-%dT%T.%3NZ`] (sys) Starting" >> /var/log/Zu-CMS/Zu-CMS.log
-end script
-
-pre-stop script
-    rm /var/run/Zu-CMS/Zu-CMS.pid
-    echo "[`date -u +%Y-%m-%dT%T.%3NZ`] (sys) Stopping" >> /var/log/Zu-CMS/Zu-CMS.log
-end script
-
-#-----[ HOWTO ]--------------------------------------------------
-# sudo initctl start Zu-CMS
-# sudo tail -f /var/log/Zu-CMS/Zu-CMS.log
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/node /srv/node-ffe-web/app/server.js -c /srv/config/node-ffe-web/config.js  >> /var/log/node-ffe-web/node-ffe-web.log 2>&1
+StandardOutput=null
+Restart=on-failure
 EOF
-
-# Startup script
-cat > /etc/init/node-ffe-web.conf <<'EOF'
-# ----------------------------------------------------------------------
-# node-ffe-web - instance
-#
-description     "node-ffe-web"
-
-start on (virtual-filesystems and net-device-up IFACE=eth0)
-stop on runlevel [06]
-
-respawn
-respawn limit 10 5    # Die if respawn more than 10 times in 5 sec.
-
-chdir /srv/node-ffe-web
-setuid ubuntu
-setgid ubuntu
-console log
-
-script
-    echo $$ > /var/run/node-ffe-web/node-ffe-web.pid
-    exec /usr/local/bin/node /srv/node-ffe-web/app/server.js -c /srv/config/node-ffe-web/config.js  >> /var/log/node-ffe-web/node-ffe-web.log 2>&1
-end script
-
-pre-start script
-    # Date format same as (new Date()).toISOString() for consistency
-    echo "[`date -u +%Y-%m-%dT%T.%3NZ`] (sys) Starting" >> /var/log/node-ffe-web/node-ffe-web.log
-end script
-
-pre-stop script
-    rm /var/run/node-ffe-web/node-ffe-web.pid
-    echo "[`date -u +%Y-%m-%dT%T.%3NZ`] (sys) Stopping" >> /var/log/node-ffe-web/node-ffe-web.log
-end script
-
-#-----[ HOWTO ]--------------------------------------------------
-# sudo initctl start node-ffe-web
-# sudo tail -f /var/log/node-ffe-web/node-ffe-web.log
-EOF
-
-
-
-# Logs to AWS Cloudwatch
-cat > /var/awslogs/etc/awslogs.conf <<'EOF'
-[general]
-state_file = /var/awslogs/state/agent-state
-
-[/var/log/Zu-CMS/Zu-CMS.log]
-datetime_format = %Y-%m-%d %H:%M:%S
-file = /var/log/Zu-CMS/Zu-CMS.log
-buffer_duration = 5000
-log_stream_name = {instance_id}
-initial_position = start_of_file
-log_group_name = api/apilog.log
-
-[/var/log/node-ffe-web/node-ffe-web.log]
-datetime_format = %Y-%m-%d %H:%M:%S
-file = /var/log/node-ffe-web/node-ffe-web.log
-buffer_duration = 5000
-log_stream_name = {instance_id}
-initial_position = start_of_file
-log_group_name = api/apilog.log
-
-EOF
-service awslogs restart
 
 # Run the application:
-service Zu-CMS start
 service node-ffe-web start
